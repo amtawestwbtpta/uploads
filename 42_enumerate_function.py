beb0dc27a742033bb4ad545902d2bbc2ba394904
amtawestwbtpta <110799204+amtawestwbtpta@users.noreply.github.com>
2025-09-29 16:24:36 +0530

    Delete file via Next.js app

--- 42_enumerate_function.py
+++
@@ -1,6 +0,0 @@
-marks = [4, 12, 36, 98, 64, 25, 49, 81, 100, 16]
-for index, mark in enumerate(marks):
-    if mark >= 50:
-        print(f"Student {index + 1} has passed with a mark of {mark}.")
-    else:
-        print(f"Student {index + 1} has failed with a mark of {mark}.")
